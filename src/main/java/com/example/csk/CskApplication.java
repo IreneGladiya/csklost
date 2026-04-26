91ffb61092b4e43a70e9913d63adf923735d8c5c
iyrin <iyrinpriyadharshni.27csa@licet.ac.in>
2026-04-26 18:26:35 +0530

    inital commit

---
+++ src/main/java/com/example/csk/CskApplication.java
@@ -0,0 +1,13 @@
+package com.example.csk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/csk/CskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
